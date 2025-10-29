84a0cedce4601f2328aaa9eeabd4233f35f5771d
jmateo-v <jmateov5@gmail.com>
2025-10-29 11:52:30 +0000

    Fixed the spawning redir problem, stronger error function to avoid segfault in echo ">>"

--- tokenizing/token_error.c
+++ tokenizing/token_error.c
@@ -6,50 +6,11 @@
 /*   By: dogs <dogs@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 15:17:53 by dogs              #+#    #+#             */
-/*   Updated: 2025/10/24 11:22:59 by dogs             ###   ########.fr       */
+/*   Updated: 2025/10/29 11:41:53 by dogs             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-int ft_check_errors(t_token *tokens, int len)
-{
-    int i = 0;
-    int has_command = 0;
-
-    while (i < len)
-    {
-        has_command = 0;
-
-        while (i < len && !(tokens[i].value[0] == '|' && tokens[i].value[1] == '\0'))
-        {
-            char *curr = tokens[i].value;
-            char *next = (i + 1 < len) ? tokens[i + 1].value : NULL;
-
-            if (!curr || curr[0] == '\0') {
-                i++;
-                continue;
-            }
-
-            if (ft_strchr("<>", curr[0])) {
-                if (!next || ft_strchr("|&", next[0]))
-                    return (ft_perror(curr, SYN_ERR), 1);
-            }
-
-            if (!ft_strchr("|&<>", curr[0]))
-                has_command = 1;
-
-            i++;
-        }
-
-        if (!has_command)
-            return (ft_perror(tokens[i].value, SYN_ERR), 1);
-
-        i++;
-    }
-
-    return 0;
-}
 
 int new_ft_check_errors(t_token *tokens, int n_tokens)
 {
@@ -63,7 +24,7 @@ int new_ft_check_errors(t_token *tokens, int n_tokens)
             return (ft_perror("(null)", ERR_EMPTY_TOKEN), 1);
         if (tokens[i].token_type == T_REDIRECTION)
         {
-            if (i + 1 >= n_tokens)
+            if (i + 1 >= n_tokens || tokens[i + 1].token_type != T_WORD)
                 return (ft_perror(tokens[i].value, ERR_MISSING_REDIR_TARGET), 1);
             
         }
